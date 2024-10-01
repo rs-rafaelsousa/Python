2a042f820c27264a3c028653baf738723ef3912a
Rafael da Silva Sousa <141595662+rs-rafaelsousa@users.noreply.github.com>
2024-10-01 15:07:03 -0300

    Q7.py

---
+++ atv/Q7.py
@@ -0,0 +1,10 @@
+import math
+
+# Armazenar o raio em uma variável
+raio = 4
+
+# Calcular o volume da esfera
+volume = (4/3) * math.pi * (raio ** 3)
+
+# Exibir o resultado
+print(f"O volume da esfera com raio {raio} é: {volume:.2f}")
